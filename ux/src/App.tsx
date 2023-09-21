b6554c48b7b7fcdefeb2d4ed7f90da5eff9ada62
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-09-21 20:23:30 -0300

    create react router #20

--- ux/src/App.tsx
+++ ux/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
-    return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    )
-}
+  return <Login></Login>;
+};
 
-export default App
+export default App;
--- ux/src/main.tsx
+++ ux/src/main.tsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './components/router.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>);
