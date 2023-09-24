0406c0a49a5461d06dda998b20972a5f3e03fb1c
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-09-24 11:26:09 -0300

    adding missing bottom tabs #49

--- mobile/src/navigation/MainTabsNavigation.tsx
+++ mobile/src/navigation/MainTabsNavigation.tsx
@@ -3,6 +3,9 @@ import { MainTabsParamList } from './types';
 import { NavigationContainer } from '@react-navigation/native';
 import GameScreen from '../screens/Game';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import StoreScreen from '../screens/Store';
+import LeaderboardScreen from '../screens/Leaderboard';
+import SettingsScreen from '../screens/Settings';
 
 const MainTabs = createBottomTabNavigator<MainTabsParamList>();
 
@@ -19,6 +22,39 @@ export default function MainTabsNavigation() {
               <Icon name="gamepad" color={color} size={size} />
             ),
             tabBarLabel: 'Jogar',
+          }}
+        />
+        <MainTabs.Screen
+          name="Store"
+          component={StoreScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="shopping-bag" color={color} size={size} />
+            ),
+            tabBarLabel: 'Loja',
+          }}
+        />
+        <MainTabs.Screen
+          name="Leaderboad"
+          component={LeaderboardScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="line-chart" color={color} size={size} />
+            ),
+            tabBarLabel: 'Placar',
+          }}
+        />
+        <MainTabs.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="cog" color={color} size={size} />
+            ),
+            tabBarLabel: 'Opções',
           }}
         />
       </MainTabs.Navigator>
